61351492e33e5f84270018997999d4a409f29355
CamDavi5 <camrdavis.biz@gmail.com>
2022-06-01 19:06:20 -0500

    Add project files.

---
+++ FunctionFun/Program.cs
@@ -0,0 +1,46 @@
+﻿static int SongDate() 
+{
+    return 1985;
+}
+
+static bool IsGood()
+{
+    return true;
+}
+
+static string ByAha()
+{
+    string chorus = TakeOnMe();
+    chorus += TakeMeOn();
+    chorus += IllBeGone();
+    chorus += InADayOrTwo();
+
+    return chorus;
+}
+
+static string TakeOnMe()
+{
+    return "Take on me";
+}
+
+static string TakeMeOn()
+{
+    return ", Take me on";
+}
+
+static string IllBeGone()
+{
+    return ", I'll be gone";
+}
+
+static string InADayOrTwo()
+{
+    return ", In a day or twooooooo";
+}
+
+string verse = ByAha();
+Console.WriteLine(verse);
+int date = SongDate();
+Console.WriteLine("When did this song release?: " + date);
+bool opinion = IsGood();
+Console.WriteLine("Do AI think this song is a banger?: " + opinion);
